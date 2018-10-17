099fd8da91a04e0fff57beb549c5481f7c237723
Raphael BRUGIER <raphael.brugier@gmail.com>
2018-10-16 21:48:10 -0400

    Add notes

--- src/main/java/helloworld/App.java
+++ src/main/java/helloworld/App.java
@@ -14,17 +14,17 @@ public class App implements RequestHandler<Object, Object> {
     static double staticRandom;
 
     static {
-        staticRandom = Math.random();
+        staticRandom = Math.random(); // (1) Value generated in the static block
     }
 
     private final double constructorRandom;
 
     public App() {
-        constructorRandom = Math.random();
+        constructorRandom = Math.random(); // (2) Value generated in the Handler constructor
     }
 
     public Object handleRequest(final Object input, final Context context) {
-        double invocationRandom = Math.random();
+        double invocationRandom = Math.random(); // (3) Value generated in the Handler method for each invocation
 
         String output = String.format(
                 "{ \"static random\": \"%f\", \"constructorRandom\": \"%f\", \"invocationRandom\": \"%f\"}",
