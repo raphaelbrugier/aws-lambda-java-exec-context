b833868734b9b702c4cd8931efbdf3fa59d21d71
Raphael BRUGIER <raphael.brugier@gmail.com>
2018-10-16 22:37:03 -0400

    Update Handler class name

--- src/main/java/helloworld/LambdaHandler.java
+++ src/main/java/helloworld/LambdaHandler.java
@@ -7,9 +7,9 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * Handler for requests to Lambda function.
+ * LambdaHandler for requests to Lambda function.
  */
-public class Handler implements RequestHandler<Object, Object> {
+public class LambdaHandler implements RequestHandler<Object, Object> {
 
     static double staticRandom;
 
@@ -19,12 +19,12 @@ public class Handler implements RequestHandler<Object, Object> {
 
     private final double constructorRandom;
 
-    public Handler() {
-        constructorRandom = Math.random(); // (2) Value generated in the Handler constructor
+    public LambdaHandler() {
+        constructorRandom = Math.random(); // (2) Value generated in the LambdaHandler constructor
     }
 
     public Object handleRequest(final Object input, final Context context) {
-        double invocationRandom = Math.random(); // (3) Value generated in the Handler method for each invocation
+        double invocationRandom = Math.random(); // (3) Value generated in the LambdaHandler method for each invocation
 
         String output = String.format(
                 "{ \"1 - static value\": \"%f\", \" 2 - constructor value\": \"%f\", \"3 - invocation value\": \"%f\"}",
